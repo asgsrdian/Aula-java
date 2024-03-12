c537af6ed76cbe333e2b96c238f63628e7855d0c
asgsrdian <r.i.beiro@hotmail.com>
2024-03-12 14:07:06 -0300

    ex14

--- src/java.java
+++ src/java.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
+import java.util.List;
 
-public class main {
+public class java {
 
     public static void main(String[] args) {
         // criando um objeto do tipo aluno
