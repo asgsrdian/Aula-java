5fc467a4328c1116512e0c8649fa830e8a0cdda9
asgsrdian <r.i.beiro@hotmail.com>
2024-03-12 15:48:51 -0300

    ex16

---
+++ src/ex19.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ex19 {
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Nota 1: ");
+        double nota1= scanner.nextDouble();
+
+        System.out.println("Nota 2: ");
+        double nota2= scanner.nextDouble();
+
+        System.out.println("Nota 3: ");
+        double nota3= scanner.nextDouble();
+
+        double media= (nota1 + nota2 + nota3) / 3;
+
+        System.out.printf("--Media: %.2f" + media);
+
+        scanner.close();
+        
+    }
+}
